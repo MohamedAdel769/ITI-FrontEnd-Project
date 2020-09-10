2959573a6447e02e735ee21de7c67cacbb9f13c2
MohamedAdel769 <mohamed.adel90211@gmail.com>
2020-09-10 11:30:04 +0200

    Added responsive header

--- script/custom.js
+++ script/custom.js
@@ -10,5 +10,14 @@ $(function (){
         myHeader.height($(window).height());
     });
     
+    var overlay = $('.overlay');
+    $(".bars").click(function(){
+        $('.overlay-content').fadeIn(800, 'linear');
+        overlay.addClass('overlay-active');
+    });
+    $(".close").click(function(){
+        $('.overlay-content').fadeOut(250);
+        overlay.removeClass('overlay-active');
+    });
 
 });
