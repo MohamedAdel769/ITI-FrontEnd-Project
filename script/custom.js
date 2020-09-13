0f0cb07d67237c1b3ec760272e31f86401b925b1
MohamedAdel769 <mohamed.adel90211@gmail.com>
2020-09-13 13:00:54 +0200

    Added WhyUs&Products sections

--- script/custom.js
+++ script/custom.js
@@ -1,23 +1,33 @@
-$(function (){
+$(function () {
+  "use strict";
 
-    'use strict';
+  // adjust height for header
+  var myHeader = $("header");
+  myHeader.height($(window).height());
 
-    // adjust height for header
-    var myHeader = $('header');
+  $(window).resize(function () {
     myHeader.height($(window).height());
+  });
 
-    $(window).resize(function(){
-        myHeader.height($(window).height());
-    });
-    
-    var overlay = $('.overlay');
-    $(".bars").click(function(){
-        $('.overlay-content').fadeIn(800, 'linear');
-        overlay.addClass('overlay-active');
-    });
-    $(".close").click(function(){
-        $('.overlay-content').fadeOut(250);
-        overlay.removeClass('overlay-active');
-    });
+  var overlay = $(".overlay");
+  $(".bars").click(function () {
+    $(".overlay-content").fadeIn(800, "linear");
+    overlay.addClass("overlay-active");
+  });
+  $(".close").click(function () {
+    $(".overlay-content").fadeOut(250);
+    overlay.removeClass("overlay-active");
+  });
 
+  var favIcon = $(".add-fav");
+  favIcon.hover(
+    function () {
+      $(this).removeClass("far");
+      $(this).addClass("fas");
+    },
+    function () {
+      $(this).removeClass("fas");
+      $(this).addClass("far");
+    }
+  );
 });
